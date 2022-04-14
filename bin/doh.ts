c6dea9c2f636f7f690339e110042dc1e2c65e990
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-04-14 14:40:37 +0200

    upgrade cdk and other node dependencies

--- bin/doh.ts
+++ bin/doh.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { App } from '@aws-cdk/core'
+import { App } from 'aws-cdk-lib'
 import { DOHStack } from '../lib/doh-stack'
 
 const app = new App()
--- lib/doh-stack.ts
+++ lib/doh-stack.ts
@@ -1,25 +1,26 @@
-import { CfnApi } from '@aws-cdk/aws-apigatewayv2'
-import { ServicePrincipal } from '@aws-cdk/aws-iam'
-import { Code, Function as Lambda, Runtime } from '@aws-cdk/aws-lambda'
-import { Construct, Stack, StackProps } from '@aws-cdk/core'
+import { Construct } from 'constructs'
+import {
+  aws_lambda,
+  Stack, StackProps,
+} from 'aws-cdk-lib'
+
+import { HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha'
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha'
 import { resolve } from 'path'
 
 export class DOHStack extends Stack {
   constructor(scope:Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    const lambda = new Lambda(this, 'DNSHandler', {
-      code: Code.fromAsset(resolve(__dirname, '../lambda/app.zip')),
+    const lambda = new aws_lambda.Function(this, 'DNSHandler', {
+      code: aws_lambda.Code.fromAsset(resolve(__dirname, '../lambda/app.zip')),
       handler: 'app',
-      runtime: Runtime.GO_1_X
+      runtime: aws_lambda.Runtime.GO_1_X
     })
 
-    lambda.grantInvoke(new ServicePrincipal('apigateway.amazonaws.com'))
-
-    new CfnApi(this, 'Resource', {
-      name: 'DOH',
-      protocolType: 'HTTP',
-      target: lambda.functionArn
+    new HttpApi(this, 'Api', {
+      apiName: 'DOH',
+      defaultIntegration: new HttpLambdaIntegration('default', lambda)
     })
   }
 }
