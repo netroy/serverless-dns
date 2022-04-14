2b42f6173607dc814ce971174f7cc961180705f4
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-04-14 15:43:20 +0200

    switch to GoFunction construct to automate the lambda build

--- README.md
+++ README.md
@@ -5,10 +5,6 @@ Unfortunately, there aren't many public DOH servers that guarantee privacy.
 
 This application uses serverless technologies from AWS to run an extremely low-cost & zero-maintaince DNS server.
 
-## Deployment
-
-Currenty this repo isn't completely automated. Please follow these steps:
-
 ### Requirements
 
 - gvm with go 1.18+
@@ -16,7 +12,6 @@ Currenty this repo isn't completely automated. Please follow these steps:
 - yarn
 - aws cli installed & configured with correct credentials
 
-### Setup
+## Deployment
 
-- run `make setup build` in the lambda folder, to prepare an uploadable zip file for AWS Lambda
 - run `yarn && yarn cdk deploy` in the root folder
--- bin/doh.ts
+++ bin/doh.ts
@@ -1,6 +1,38 @@
 #!/usr/bin/env node
 import { App } from 'aws-cdk-lib'
-import { DOHStack } from '../lib/doh-stack'
+import { Construct } from 'constructs'
+import { Stack } from 'aws-cdk-lib'
+import { GoFunction } from '@aws-cdk/aws-lambda-go-alpha'
+import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha'
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha'
+
+const name = 'DoH'
+
+class DoHStack extends Stack {
+  constructor(scope:Construct) {
+    super(scope, name)
+
+    const lambda = new GoFunction(this, 'Handler', {
+      functionName: name,
+      entry: 'lambda',
+    })
+
+    const api = new HttpApi(this, 'Api', {
+      apiName: name,
+    })
+
+    // TODO: add support for JSON API for DoH
+    api.addRoutes({
+      methods: [
+        HttpMethod.GET,
+        // HttpMethod.POST, // TODO: Add support for POST
+      ],
+      path: '/dns-query',
+      integration: new HttpLambdaIntegration('dns-query', lambda),
+    })
+  }
+}
+
 
 const app = new App()
-new DOHStack(app, 'DOHStack')
+new DoHStack(app)
